59482fa74d10b833981e595fb42ba4572e11ebde
Nick Simpkins <nsimpkins@hawk.iit.edu>
2023-04-07 21:22:42 -0500

    Added JS email verification

---
+++ js/index.js
@@ -0,0 +1,12 @@
+jQuery(function($) {
+    $('html').removeClass('nojs');
+    $('html').addClass('hasjs');
+  });
+
+function ValidateEmail(email) {
+   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)) {
+      return (true)
+    }
+      alert("You have entered an invalid email address!")
+      return (false)
+  }
